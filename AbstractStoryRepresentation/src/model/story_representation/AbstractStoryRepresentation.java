81d03272c426487bc9ff541c7320b1c45df740d0
Gege <cugeraldine@yahoo.com>
2016-06-24 13:35:54 +0800

    log the frames for easy tracing

--- AbstractStoryRepresentation/src/model/story_representation/AbstractStoryRepresentation.java
+++ AbstractStoryRepresentation/src/model/story_representation/AbstractStoryRepresentation.java
@@ -5,13 +5,23 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.jena.atlas.logging.Log;
+import org.apache.log4j.Logger;
 
 import model.story_representation.story_element.Conflict;
 import model.story_representation.story_element.Resolution;
 import model.story_representation.story_element.noun.Noun;
+import model.story_representation.story_element.story_sentence.Description;
+import model.story_representation.story_element.story_sentence.Event;
 import model.story_representation.story_element.story_sentence.StorySentence;
+import model.text_generation.DirectivesGenerator;
 
 public class AbstractStoryRepresentation {
+	
+	private static Logger log = Logger
+			.getLogger(AbstractStoryRepresentation.class.getName());
 
 	private Map<String, List<StorySentence>> storySentences;
 
@@ -53,7 +63,146 @@ public class AbstractStoryRepresentation {
 		}
 
 		storySentences.add(storySentence);
+		
+		log.debug("part of story: " + this.partOfStory);
+		
+		for (Event p : storySentence.getManyPredicates().values()) {
+			log.debug("doers: ");
+			for (Map.Entry<String, Noun> entry : p
+					.getManyDoers().entrySet()) {
+				log.debug(
+						"id: " + entry.getValue().getId());
+				log.debug("common noun? "
+						+ entry.getValue().getIsCommon());
 
+				log.debug("doers' attributes: ");
+				for (Map.Entry<String, List<String>> entry2 : entry
+						.getValue().getAttributes()
+						.entrySet()) {
+					log.debug(entry2.getKey() + " ");
+					log.debug(entry2.getValue());
+					
+				}
+
+				log.debug("doers' references: ");
+				for (Map.Entry<String, Map<String, Noun>> entry2 : entry
+						.getValue().getReferences()
+						.entrySet()) {
+					log.debug(entry2.getKey() + " ");
+					for (Noun n : entry2.getValue().values()) {
+						log.debug(n.getId() + " ");
+					}
+					
+				}
+			}
+
+			log.debug("action: " + p.getVerb().getAction());
+			log.debug("isNegated: " + p.getVerb().isNegated());
+
+			log.debug("receivers: ");
+			for (Map.Entry<String, Noun> entry2 : p
+					.getReceivers().entrySet()) {
+				log.debug(
+						"id: " + entry2.getValue().getId());
+				log.debug("common noun? "
+						+ entry2.getValue().getIsCommon());
+
+				log.debug("receiver's attributes");
+				for (Map.Entry<String, List<String>> entry3 : entry2
+						.getValue().getAttributes()
+						.entrySet()) {
+					log.debug(entry3.getKey() + " ");
+					log.debug(entry3.getValue() + " ");
+					
+				}
+
+				log.debug("receiver's references");
+				for (Map.Entry<String, Map<String, Noun>> entry3 : entry2
+						.getValue().getReferences()
+						.entrySet()) {
+					log.debug(entry3.getKey() + " ");
+					for (Noun n : entry3.getValue().values()) {
+						log.debug(n.getId() + " ");
+					}
+					
+				}
+			}
+
+			log.debug("dobjs: ");
+			for (Map.Entry<String, Noun> entry3 : p
+					.getDirectObjects().entrySet()) {
+				log.debug(
+						"id: " + entry3.getValue().getId());
+				log.debug("common noun? "
+						+ entry3.getValue().getIsCommon());
+
+				log.debug("dobj's attributes ");
+				for (Map.Entry<String, List<String>> entry4 : entry3
+						.getValue().getAttributes()
+						.entrySet()) {
+					log.debug(entry4.getKey() + " ");
+					log.debug(entry4.getValue() + " ");
+					
+				}
+
+				log.debug("dobj's references ");
+				for (Map.Entry<String, Map<String, Noun>> entry4 : entry3
+						.getValue().getReferences()
+						.entrySet()) {
+					log.debug(entry4.getKey() + " ");
+					for (Noun n : entry4.getValue().values()) {
+						log.debug(n.getId() + " ");
+					}
+					
+				}
+			}
+		}
+		
+		log.debug("descriptions ");
+
+		for (Entry<String, Description> entry : storySentence
+				.getManyDescriptions().entrySet()) {
+			//log.debug(entry.getKey());
+
+			for (Map.Entry<String, Noun> doer : entry.getValue()
+					.getManyDoers().entrySet()) {
+				log.debug(doer.getValue().getId());
+			}
+
+			log.debug("attributes ");
+			for (Entry<String, List<String>> entry2 : entry
+					.getValue().getAttributes().entrySet()) {
+				log.debug(entry2.getKey() + " ");
+				log.debug(entry2.getValue() + " ");
+			}
+
+			log.debug("references ");
+			for (Entry<String, Map<String, Noun>> entry2 : entry
+					.getValue().getReferences().entrySet()) {
+				log.debug(entry2.getKey() + " ");
+				for (Noun n : entry2.getValue().values()) {
+					log.debug(n.getId() + " ");
+				}
+				
+			}
+		}
+
+		for (Event predicate : storySentence.getManyPredicates().values()) {
+			log.debug(
+					"p_concepts: " + predicate.getConcepts());
+			log.debug(
+					" polarity: " + predicate.getPolarity());
+		}
+		for (Description description : storySentence.getManyDescriptions()
+				.values()) {
+			log.debug(
+					"n_concepts: " + description.getConcepts());
+			log.debug(
+					" polarity: " + description.getPolarity());
+		}
+		
+		log.debug("\n");
+	
 	}
 
 	public StorySentence getCurrentStorySentence() {
