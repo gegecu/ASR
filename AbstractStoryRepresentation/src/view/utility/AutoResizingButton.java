70d59ed9970e0d289d071c10b664fd2a32f9e2cb
hans123456 <h.g.n.chua@gmail.com>
2016-07-29 23:23:22 +0800

    cross out instead of line

--- AbstractStoryRepresentation/src/view/utility/AutoResizingButton.java
+++ AbstractStoryRepresentation/src/view/utility/AutoResizingButton.java
@@ -7,6 +7,7 @@ import java.awt.BasicStroke;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Line2D;
@@ -37,6 +38,9 @@ public class AutoResizingButton extends JButton {
 	private boolean fontChanged = true;
 	private boolean checked = false;
 	private List<DescriptionDialog> descriptionDialogs;
+
+	private static float checkedStroke = 5f;
+	private static float checkedStroke_2 = checkedStroke / 2;
 
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -80,8 +84,12 @@ public class AutoResizingButton extends JButton {
 		if (checked) {
 
 			Graphics2D g2 = ((Graphics2D) g);
-			g2.setStroke(new BasicStroke(5));
-			Line2D line = new Line2D.Float(0, height / 2, width, height / 2);
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+			g2.setStroke(new BasicStroke(checkedStroke));
+			Line2D line = new Line2D.Float(0, checkedStroke_2, width, height - checkedStroke_2);
+			g2.draw(line);
+			line = new Line2D.Float(0, height - checkedStroke_2, width, checkedStroke_2);
 			g2.draw(line);
 
 		}
@@ -97,11 +105,10 @@ public class AutoResizingButton extends JButton {
 	public void setChecked(boolean checked) {
 		this.checked = checked;
 	}
-	
+
 	public boolean isChecked() {
 		return checked;
 	}
-
 
 	public void addDescription(String title, String description) {
 
