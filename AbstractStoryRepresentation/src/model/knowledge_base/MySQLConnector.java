a37f6f4d0272dbbe80d840f011099e69541949f4
hans123456 <h.g.n.chua@gmail.com>
2016-08-19 20:03:59 +0800

    moved mysql properties to file

--- AbstractStoryRepresentation/src/model/knowledge_base/MySQLConnector.java
+++ AbstractStoryRepresentation/src/model/knowledge_base/MySQLConnector.java
@@ -1,27 +1,54 @@
 package model.knowledge_base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import com.mysql.jdbc.Connection;
 
 public class MySQLConnector {
 
-	private String url = "jdbc:mysql://localhost:3306/";
-	private String dbName = "alice?autoReconnect=true&useSSL=false";
-	private String driver = "com.mysql.jdbc.Driver";
-	private String userName = "root";
-	private String password = "1234";
-	private Connection connection;
-	private static MySQLConnector db = new MySQLConnector();
+	private static String driver = "com.mysql.jdbc.Driver";
+	private static String url;
+	private static String dbName;
+	private static String userName;
+	private static String password;
+	private static MySQLConnector db;
 
 	private MySQLConnector() {
+
+	}
+
+	static {
 		try {
+
 			Class.forName(driver).newInstance();
-		} catch (InstantiationException e1) {
-		} catch (IllegalAccessException e2) {
-		} catch (ClassNotFoundException e3) {
+
+			Properties props = new Properties();
+			FileInputStream in = new FileInputStream("files/mysql.properties");
+			props.load(in);
+			in.close();
+
+			url = props.getProperty("mysql.url");
+			dbName = props.getProperty("mysql.database_name");
+			userName = props.getProperty("mysql.username");
+			password = props.getProperty("mysql.password");
+
+			db = new MySQLConnector();
+
+		} catch (InstantiationException e) {
+		} catch (IllegalAccessException e) {
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
 	}
 
 	public static MySQLConnector getInstance() {
@@ -29,9 +56,8 @@ public class MySQLConnector {
 	}
 
 	public Connection getConnection() throws SQLException {
-		this.connection = (Connection) DriverManager.getConnection(url + dbName,
-				userName, password);
-		return connection;
+		return (Connection) DriverManager.getConnection(url + dbName, userName,
+				password);
 	}
 
 }
