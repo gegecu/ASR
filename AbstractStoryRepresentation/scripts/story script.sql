d3830f81e6ad5f4968f4fa932b6372cf4d964cd1
hans123456 <h.g.n.chua@gmail.com>
2016-08-26 03:34:04 +0800

    removed logs, finalize Alice.sql

--- AbstractStoryRepresentation/scripts/story script.sql
+++
@@ -1,9 +0,0 @@
-use `alice`;
-
-create table if not exists `alice`.`stories` (
-	`id` int unsigned auto_increment not null,
-	`title` varchar(100) not null,
-	`text` varchar(1000) not null,
-	`deleted` boolean default false,
-	primary key (`id`)
-);
--- AbstractStoryRepresentation/src/utility/EvaluationLog.java
+++ AbstractStoryRepresentation/src/utility/EvaluationLog.java
@@ -6,31 +6,7 @@ import java.io.PrintWriter;
 
 public class EvaluationLog {
 
-	private static String fileDir = "log/Evaluation Log/";
-	private static String filename = "log/Evaluation Log/Eval Log";
-	private static PrintWriter pw;
-
-	static {
-		File file = new File(fileDir);
-		if (!(file.exists())) {
-			file.mkdirs();
-		}
-		for (int i = 0;; i++) {
-			String name = filename + " " + i + ".txt";
-			file = new File(name);
-			if (file.exists())
-				continue;
-			try {
-				pw = new PrintWriter(new FileWriter(file), true);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			break;
-		}
-	}
-
 	public static void log(String string) {
-		pw.println(string);
 	}
 
 }
